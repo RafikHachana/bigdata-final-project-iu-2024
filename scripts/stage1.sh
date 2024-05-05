1e3155c9474201e4ff5af2124304649798d075e3
Rafik Hachana <rafikhachana@gmail.com>
2024-05-06 02:33:12 +0300

    download data

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+
+#url="https://archive.ics.uci.edu/static/public/791/metropt+3+dataset.zip"
+
+#wget "$url" -O data/data.zip
+
+#unzip data/data.zip -d data/
+
+# rm data/data.zip
+mv "data/MetroPT3(AirCompressor).csv" data/MetroPT3.csv
+
